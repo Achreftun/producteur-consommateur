c82afacc7b653422a1e8b8c44050eb1b57af206f
el mouelhi <elmouelhi.achref@gmail.com>
2020-11-14 07:36:03 +0100

    Installing filters

--- projet-pizza/src/org/eclipse/controller/DeconnexionServlet.java
+++ projet-pizza/src/org/eclipse/controller/DeconnexionServlet.java
@@ -17,7 +17,7 @@ public class DeconnexionServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		session.invalidate();
-		getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+		response.sendRedirect("connexion");
 
 	}
 
